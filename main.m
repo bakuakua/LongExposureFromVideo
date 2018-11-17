1a9327292f233a309cc41920aa7c957bc56c1016
bakuakua <fjiang4@emory.edu>
2018-11-16 21:35:40 -0500

    initial commit: videoToFrame and main added

---
+++ main.m
@@ -0,0 +1,33 @@
+% This script encapsulates the whole long exposure creation pipeline
+clc;    % Clear the command window.
+close all;  % Close all figures (except those of imtool.)
+imtool close all;  % Close all imtool figures.
+clear;  % Erase all existing variables.
+workspace;  % Make sure the workspace panel is showing.
+fontSize = 22;
+VideoPath = 'Videos/P7230137.MOV'; %path to input video
+[Frames,info] = videoToFrames('VideoPath');
+%% image stablization
+% this section of the pipeline intends to remove the movement of the
+% background to create pre-processed frames with static background
+
+
+
+
+
+%% create Long exposure image from frames
+% this section of the pipeline intends to create a simulated long exposure
+% image from frames.
+
+
+
+
+%% moving object detection and removal
+% 
+
+
+
+
+
+
+
