20f7b0f84e8421f5e37273b2b51a8d91fd9c1627
Huilin <kss221@foxmail.com>
2018-11-18 19:09:30 -0500

    bug fixed

--- main.m
+++ main.m
@@ -28,27 +28,7 @@ VideoPath = 'Videos/P7230137.MOV'; %path to input video
 
 downsample = 5;                      
 ObjRemoval(Frames,info,downsample)
-%%
-VideoPath = 'Videos/night_car2.MOV';
-obj = VideoReader(VideoPath);
-a = read(obj,[1 200]);
-FrameCount = 200;
-detector = vision.ForegroundDetector(...
-       'NumTrainingFrames', 5, ...
-       'InitialVariance', 30*30);
-rgbSum = 0;
-for fr = 1:FrameCount
-    frame = read(obj,fr);
-    for i = 1:3
-        fgMask(:,:,i) = detector.step(frame(:,:,i));
-        bgMask(:,:,i) = 1- fgMask(:,:,i);
-        frameNo(:,:,i) = double(frame(:,:,i)) .* double(1-bgMask(:,:,i));
-        thisFrame(:,:,i) = double(frameNo(:,:,i));
-    end
-    rgbSum = rgbSum + thisFrame;
-end
-rgbMean = rgbSum / FrameCount;
-imshow(uint8(rgbMean));
+
 
 
 
